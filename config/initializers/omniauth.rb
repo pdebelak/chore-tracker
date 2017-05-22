a7b05390b028d00c2d26c09648c36dd4e8320a6c
pdebelak <pdebelak@gmail.com>
2017-05-21 20:31:59 -0500

    Make mildly mobile friendly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], redirect_uri: ENV["GOOGLE_AUTH_REDIRECT_URI"] || "http://localhost:3000/auth/google_oauth2/callback"
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
